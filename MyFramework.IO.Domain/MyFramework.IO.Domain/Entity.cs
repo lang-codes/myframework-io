30403b47ae612a0cbc09ae2b74c1837aa7382f70
lang-codes <lang.codes@gmail.com>
2020-04-13 21:44:30 -0300

    [lang-codes] Creating entity class to domain.

---
+++ MyFramework.IO.Domain/MyFramework.IO.Domain/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyFramework.IO.Domain
+{
+    public abstract class Entity
+    {
+        private const int saltHashNumber = 909;
+
+        //TODO: Create Validations
+
+        public Guid Id { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var entitty = obj as Entity;
+
+            if (ReferenceEquals(this, entitty))
+                return true;
+
+            if (ReferenceEquals(null, entitty))
+                return false;
+
+            return Id.Equals(entitty.Id);
+        }
+
+        public static bool operator ==(Entity entityA, Entity entityB)
+        {
+            if (ReferenceEquals(entityA, null) && ReferenceEquals(entityB, null))
+                return true;
+
+            if (ReferenceEquals(entityA, null) || ReferenceEquals(entityB, null))
+                return false;
+
+            return entityA.Equals(entityB);
+        }
+
+        public static bool operator !=(Entity entityA, Entity entityB)
+        {
+            return !(entityA == entityB);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * saltHashNumber) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}[Id={Id}]";
+        }
+    }
+}
